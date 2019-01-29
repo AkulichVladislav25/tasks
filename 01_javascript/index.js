7f54bb1d0653678d3c8e795e668af4fa47457da7
Vlad <akulich.vladislav25@gmail.com>
2019-01-29 21:32:25 +0300

    Make func calcTime, reduce minus,plus, stop

--- 01_javascript/index.js
+++ 01_javascript/index.js
@@ -9,29 +9,60 @@ const buttons = {
 };
 
 timer.textContent = '00:00';
+let startDate = new Date(),
+    marker = false;
 
-function startTIME(startDate) {
-    alert(startDate);
-    var thisDate = new Date();
-    var t = thisDate.getTime() - startDate;
-
+function startTIME() {
+    let thisDate = new Date();
+    let t = thisDate.getTime() - startDate;
     t = Math.floor(t / 1000);
-    var s = t % 60;
+    let s = t % 60;
     t -= s;
     t = Math.floor(t / 60);
-    var m = t % 60;
+    let m = t % 60;
     t -= m;
     t = Math.floor(t / 60);
-    var h = t % 60;
+    let h = t % 60;
     if (h < 10) h = h;
     if (m < 10) m = m;
     if (s < 10) s = '0' + s;
     timer.textContent = h.toString() + m.toString() + ':' + s.toString();
-
 };
-
-
 buttons.start.addEventListener('click', event => {
-    setTimeout('startTIME()', 1000, new Date());
-
+    marker = false;
+    let timerId = setInterval(function startTime() {
+        if (marker) {
+            clearInterval(timerId);
+        }
+        let thisDate = new Date();
+        let t = thisDate.getTime() - startDate.getTime();
+        t = Math.floor(t / 1000);
+        let s = t % 60;
+        t -= s;
+        t = Math.floor(t / 60);
+        let m = t % 60;
+        t -= m;
+        t = Math.floor(t / 60);
+        let h = t % 60;
+        if (h < 10) h = h;
+        if (m < 10) m = m;
+        if (s < 10) s = '0' + s;
+        timer.textContent = h.toString() + m.toString() + ':' + s.toString();
+    }, 1000);
+});
+buttons.reset.addEventListener('click', event => {
+    timer.textContent = '00:00';
+    startDate = new Date();
+    marker = true;
+});
+buttons.stop.addEventListener('click', event => {
+    marker = true;
+    startDatePause = startDate;
+});
+buttons.minus.addEventListener('click', event => {
+    startDate += 10000;
+    if (startDate.getTime() < 0) startdate = 0;
+});
+buttons.plus.addEventListener('click', event => {
+    startDate -= 10000;
 });
