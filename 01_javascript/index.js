7b46b5af8129248c190499872c08cccb81ba8f43
Vlad <akulich.vladislav25@gmail.com>
2019-01-29 22:58:19 +0300

    Problems with startTime

--- 01_javascript/index.js
+++ 01_javascript/index.js
@@ -7,35 +7,15 @@ const buttons = {
     plus: document.querySelector('.plus.button'),
     minus: document.querySelector('.minus.button'),
 };
-
 timer.textContent = '00:00';
-let startDate = new Date(),
-    marker = false;
-
-function startTIME() {
-    let thisDate = new Date();
-    let t = thisDate.getTime() - startDate;
-    t = Math.floor(t / 1000);
-    let s = t % 60;
-    t -= s;
-    t = Math.floor(t / 60);
-    let m = t % 60;
-    t -= m;
-    t = Math.floor(t / 60);
-    let h = t % 60;
-    if (h < 10) h = h;
-    if (m < 10) m = m;
-    if (s < 10) s = '0' + s;
-    timer.textContent = h.toString() + m.toString() + ':' + s.toString();
-};
+let startDate = new Date();
+let isTimerRunning = false;
 buttons.start.addEventListener('click', event => {
-    marker = false;
-    let timerId = setInterval(function startTime() {
-        if (marker) {
-            clearInterval(timerId);
-        }
+    isTimerRunning = false;
+    timerId = setInterval(function startTime() {
+        if (isTimerRunning) clearInterval(timerId);
         let thisDate = new Date();
-        let t = thisDate.getTime() - startDate.getTime();
+        let t = thisDate.getTime() - startDate;
         t = Math.floor(t / 1000);
         let s = t % 60;
         t -= s;
@@ -52,16 +32,17 @@ buttons.start.addEventListener('click', event => {
 });
 buttons.reset.addEventListener('click', event => {
     timer.textContent = '00:00';
+    isTimerRunning = true;
     startDate = new Date();
-    marker = true;
 });
 buttons.stop.addEventListener('click', event => {
-    marker = true;
-    startDatePause = startDate;
+    isTimerRunning = true;
 });
 buttons.minus.addEventListener('click', event => {
-    startDate += 10000;
-    if (startDate.getTime() < 0) startdate = 0;
+    let testDate = new Date();
+    if (testDate - startDate < 10000) { startdate = 0; } else {
+        startDate += 10000;
+    }
 });
 buttons.plus.addEventListener('click', event => {
     startDate -= 10000;
